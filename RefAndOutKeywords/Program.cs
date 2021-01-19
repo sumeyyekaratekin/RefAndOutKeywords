14b386c8f168c10361681bcb4b1011299c820953
Sümeyye Karatekin <sumeyyekaratekin96@gmail.com>
2021-01-19 15:30:14 +0300

    Add project files.

---
+++ RefAndOutKeywords/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RefAndOutKeywords
+{
+ 
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 15;
+            int sonuc1 = Carpma(x, y);
+
+            Console.WriteLine("Başlangıçta ;  5 * 15  = "+sonuc1); // bu satırda 10 çıktısını alırız. 
+                                   //Çünkü biz değer tipli olarak methoda göndermiş olduk.
+            
+           
+            Console.WriteLine("\nRef anahtar kelimesi ile ");
+            int a = 1; 
+            int b = 5;
+            int sonuc2 = Carpma1(ref a, b);
+            Console.WriteLine("1 * 5 = "+sonuc2); //burda ref keywordu ile a 'yı referans tipine dönüştürmüş olduk bu nedenle çıktımız a'nın değerini  kullanacaktır
+                                  // cıktı:10 olacaktır.
+
+
+            Console.WriteLine("a degeri :" +a); // ref keyworduyle beraber a değeri 2 olmuş olacak.
+
+            Console.WriteLine("\nOut anahtar kelimesi ile ");
+
+            int birinci; // birinciyi out ile kullandığımız için burda setleme yapmaya ihtiyacımız olmayacaktır.
+            int ikinci = 6;
+            int sonuc3 = Carpma2(out birinci, ref ikinci);
+            Console.WriteLine("buranının sonucu : "+sonuc3); // burda da hem ref hemde out olarak kullandık verilerimizi.
+            Console.WriteLine("birinci deger : "+birinci +"  ikinci deger : "+ikinci);
+          // burda birinci deger 2 , ikinci deger 5 çıktı olarak verilecektir.
+
+        }
+        static int Carpma(int number1, int number2)
+        {
+            number1 = 2;
+            number2 = 5;
+            int sonuc = number1 * number2;
+            return sonuc;
+        }
+        static int Carpma1(ref int number1, int number2)
+        {
+            number1 = 2;
+            number2 = 5;
+            int sonuc = number1 * number2;
+            return sonuc;
+        }
+        static int Carpma2(out int number1, ref int number2)
+        {
+            number1 = 2; // out için burda setleme yapmak zorundayız.
+            number2 = 5;
+            int sonuc = number1 * number2;
+            return sonuc;
+        }
+    }
+}
+
+  
